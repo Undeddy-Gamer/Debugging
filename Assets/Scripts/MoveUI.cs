35ac2c0ba48b0c030b39152116a2209ccd2de85a
Undeddy-Gamer <ed.dixen@gmail.com>
2019-09-11 13:16:39 +1000

    debugged

---
+++ Assets/Scripts/MoveUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUI : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        Vector2 src = new Vector2(Screen.width / 16, Screen.height / 9);
+        //GUI.Box(new Rect(src.x, src.y, src.x, src.y), "");
+
+        for (int x = 0; x < 16; x++)
+        {
+            for (int y = 0; x < 16; y++)
+            {
+                GUI.Box(new Rect(x, y, src.x, src.y), "");
+            }
+        }
+    }
+}
